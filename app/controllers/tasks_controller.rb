2f4454188637513c081bd6fac071e96cd7286682
dionomusuko <dandybird1017@gmail.com>
2019-08-06 22:55:50 +0900

    add_delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,9 +12,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
-
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
+
   def create
     task = Task.new(task_params)
     if task.save
